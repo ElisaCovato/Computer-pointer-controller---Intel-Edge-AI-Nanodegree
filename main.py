f0e840fb6e1e9d85f39aca4d44612325463953d5
Elisa Covato <elisacovato@Elisas-MacBook-Pro.local>
2020-06-26 11:46:22 +0100

    Add stats preview command line

--- main.py
+++ main.py
@@ -37,13 +37,14 @@ def build_argparser():
     parser.add_argument("-prev", "--flags_preview", nargs="+", default=[],
                         help="Show models detection outputs. Add 'fm' for face detection,"
                              "lm for landmarks, pm for head pose, gm for gaze estimation,"
-                             "vo for video only output without models detection output. "
+                             "vo for video only output without models detection output, "
+                             "stats for displaying live inference time."
                              "Flags needs to be separated by space.")
     parser.add_argument("-m_prec", "--mouse_precision", type=str, default='high',
                         help="Specify mouse precision (how much the mouse moves): 'high', 'medium', 'low'."
                              "Default is high.")
     parser.add_argument("-m_speed", "--mouse_speed", type=str, default='immediate',
-                        help="Specify mouse speed (how many secs before it moves): 'immediate'(0s), 'fast'(0.1s)," 
+                        help="Specify mouse speed (how many secs before it moves): 'immediate'(0s), 'fast'(0.1s),"
                              "'medium'(0.5s) and 'slow'(1s). Default is immediate.")
     parser.add_argument("-prob", "--prob_threshold", type=float, default=0.5,
                         help="Probability threshold for detection filtering (0.5 by default)")
@@ -54,7 +55,6 @@ def build_argparser():
                         help="Add this flag to specify synchronous inference. Default false: "
                              "Asynchronous inference will be performed.")
 
-
     return parser
 
 
@@ -62,7 +62,7 @@ def main():
     # Grab command line arguments
     global preview
     args = build_argparser().parse_args()
-    print(args.sync_mode)
+
     # Get input
     input_path = args.input
     flip = False
@@ -122,109 +122,126 @@ def main():
     gm.load_model()
 
     total_models_load_time = time.time() - start_models_load_time
-    print("Total models load time: ", total_models_load_time, "ms")
+    print("Model loading time:", round(total_models_load_time, 2), "s")
 
     # Check preview flags
     if len(args.flags_preview) != 0:
         for flag in args.flags_preview:
-            if not flag in ['lm', 'pm', 'fm', 'gm', 'vo']:
+            if not flag in ['lm', 'pm', 'fm', 'gm', 'vo', 'stats']:
                 log.error("Flag '" + flag + "' is not a valid preview flag.")
                 sys.exit(1)
 
     # Initialize mouse controller
     mouse = MouseController(precision=args.mouse_precision, speed=args.mouse_speed)
     # out = cv2.VideoWriter("test.mp4", cv2.VideoWriter_fourcc(*"MP4V"), 30,  (1920, 1080), True)
-    for frame in feed.next_batch():
-        if frame is None:
-            break
-        key_pressed = cv2.waitKey(60)
 
-        if len(args.flags_preview) != 0:
-            preview = ShowPreview(frame, flip)
+    counter = 0
+    # Start inference
+    try:
+        for frame in feed.next_batch():
+            if frame is None:
+                break
+            key_pressed = cv2.waitKey(60)
 
+            counter += 1
+            start_infer_time = time.time()
 
-        # 1: Detect face
-        face_coords, crop_face = fm.predict(frame.copy())
+            # Start preview if requested by user
+            if len(args.flags_preview) != 0:
+                preview = ShowPreview(frame, flip)
 
-        # If face is not detect show a message:
-        if len(face_coords) == 0:
-            text = "No face detected"
-            log.error(text)
+            # 1: Detect face
+            face_coords, crop_face = fm.predict(frame.copy())
 
-        # If face is detect move on with next detections:
-        else:
-            # Draw face detection if applicable
-            if 'fm' in args.flags_preview:
-                preview.draw_face_box(face_coords[0], face_coords[1], face_coords[2], face_coords[3])
-
-            # 2a: Detect left and right eye
-            eyes_coord, crop_left, crop_right = lm.predict(crop_face.copy())
-            # Draw eyes detection if applicable
-            if len(eyes_coord) != 0 and 'lm' in args.flags_preview:
-                square_size = int(crop_face.shape[1] / 10 + 5)
-                # Draw left yes bounding box
-                preview.draw_eye_box(eyes_coord[:2], face_coords, square_size)
-                # Draw right eye bounding box
-                preview.draw_eye_box(eyes_coord[2:4], face_coords, square_size)
-
-            # 2b: Estimate head pose
-            angle_poses = pm.predict(crop_face.copy())
-            # Draw head pose estimation if applicable
-            if len(angle_poses) != 0 and 'pm' in args.flags_preview:
-                # Get center of face bounding box
-                xcenter = int(face_coords[0] + crop_face.shape[1] / 2)
-                ycenter = int(face_coords[1] + crop_face.shape[0] / 2)
-                preview.draw_head_pose(
-                    yaw=angle_poses[0],
-                    pitch=angle_poses[1],
-                    roll=angle_poses[2],
-                    anchor_point=(xcenter, ycenter)
-                )
-
-            # 3: Get Gaze Estimation
-            if len(crop_left) != 0 and len(crop_right) != 0:
-                gaze = gm.predict(crop_left.copy(), crop_right.copy(), angle_poses.copy())
-                # Draw gaze estimation if applicable
-                if len(gaze) != 0 and 'gm' in args.flags_preview:
-                    gaze_len = (0.4 * crop_face.shape[1], 0.4 * crop_face.shape[0])
-                    # Draw left eye gaze
-                    preview.draw_eye_gaze(eyes_coord[:2], face_coords, gaze, gaze_len)
-                    # Draw right eye gaze
-                    preview.draw_eye_gaze(eyes_coord[2:4], face_coords, gaze, gaze_len)
-
-                # Move mouse
-                if len(gaze) != 0:
-                    if flip:  # if it is a video cam stream, flip pointer direction
-                        mouse.move(-gaze[0], gaze[1])
-                    else:
-                        mouse.move(gaze[0], gaze[1])
-
-        # out.write(preview)
-        if len(args.flags_preview) != 0:
-            frame = cv2.resize(frame, (600, 600))
-            if flip:
-                frame = cv2.flip(frame, 1)
+            # If face is not detect show a message:
             if len(face_coords) == 0:
                 text = "No face detected"
+                log.error(text)
+
+            # If face is detect move on with next detections:
+            else:
+                # Draw face detection if applicable
+                if 'fm' in args.flags_preview:
+                    preview.draw_face_box(face_coords[0], face_coords[1], face_coords[2], face_coords[3])
+
+                # 2a: Detect left and right eye
+                eyes_coord, crop_left, crop_right = lm.predict(crop_face.copy())
+                # Draw eyes detection if applicable
+                if len(eyes_coord) != 0 and 'lm' in args.flags_preview:
+                    square_size = int(crop_face.shape[1] / 10 + 5)
+                    # Draw left yes bounding box
+                    preview.draw_eye_box(eyes_coord[:2], face_coords, square_size)
+                    # Draw right eye bounding box
+                    preview.draw_eye_box(eyes_coord[2:4], face_coords, square_size)
+
+                # 2b: Estimate head pose
+                angle_poses = pm.predict(crop_face.copy())
+                # Draw head pose estimation if applicable
+                if len(angle_poses) != 0 and 'pm' in args.flags_preview:
+                    # Get center of face bounding box
+                    xcenter = int(face_coords[0] + crop_face.shape[1] / 2)
+                    ycenter = int(face_coords[1] + crop_face.shape[0] / 2)
+                    preview.draw_head_pose(
+                        yaw=angle_poses[0],
+                        pitch=angle_poses[1],
+                        roll=angle_poses[2],
+                        anchor_point=(xcenter, ycenter))
+
+                # 3: Get Gaze Estimation
+                if len(crop_left) != 0 and len(crop_right) != 0:
+                    gaze = gm.predict(crop_left.copy(), crop_right.copy(), angle_poses.copy())
+                    # Draw gaze estimation if applicable
+                    if len(gaze) != 0 and 'gm' in args.flags_preview:
+                        gaze_len = (0.4 * crop_face.shape[1], 0.4 * crop_face.shape[0])
+                        # Draw left eye gaze
+                        preview.draw_eye_gaze(eyes_coord[:2], face_coords, gaze, gaze_len)
+                        # Draw right eye gaze
+                        preview.draw_eye_gaze(eyes_coord[2:4], face_coords, gaze, gaze_len)
+
+                    # Move mouse
+                    if len(gaze) != 0:
+                        if flip:  # if it is a video cam stream, flip pointer direction
+                            mouse.move(-gaze[0], gaze[1])
+                        else:
+                            mouse.move(gaze[0], gaze[1])
+
+            # Compute inference time
+            inference_time = time.time() - start_infer_time
+            text_infer = "Inference time: {:.2f}ms".format(inference_time * 1000)
+            print(text_infer)
+
+            # out.write(preview)
+            # Preview detection if applicable
+            if len(args.flags_preview) != 0:
+                frame = cv2.resize(frame, (600, 600))
                 font = cv2.FONT_HERSHEY_COMPLEX
-                textsize = cv2.getTextSize(text, font, 1, 2)[0]
-                textX, textY = int((600 - textsize[0]) / 2), int((600 + textsize[1]) / 2)
-                cv2.putText(frame, text, (textX, textY), font, 1, (200, 10, 10), 1)
-                cv2.rectangle(frame, (textX - 10, textY - textsize[1] - 10),
-                              (textX + textsize[0] + 10, textY + 10),
-                              (145, 50, 255), 2)
+                if flip:
+                    frame = cv2.flip(frame, 1)
+                if len(face_coords) == 0:
+                    text = "No face detected"
+                    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+                    textX, textY = int((600 - textsize[0]) / 2), int((600 + textsize[1]) / 2)
+                    cv2.putText(frame, text, (textX, textY), font, 1, (200, 10, 10), 1)
+                    cv2.rectangle(frame, (textX - 10, textY - textsize[1] - 10),
+                                  (textX + textsize[0] + 10, textY + 10),
+                                  (145, 50, 255), 2)
+                if 'stats' in args.flags_preview:
+                    cv2.putText(frame, text_infer, (15, 35), font, 1, (200, 10, 10), 1)
 
-            cv2.imshow("Preview", frame)
-            cv2.moveWindow("Preview", 70, 70)
+
+                cv2.imshow("Preview", frame)
+                cv2.moveWindow("Preview", 70, 70)
+
+            if key_pressed == 27:
+                break
+
+        log.warning("VideoStream ended...")
+        cv2.destroyAllWindows()
+        feed.close()
 
 
-
-        if key_pressed == 27:
-            break
-
-    log.error("VideoStream ended...")
-    cv2.destroyAllWindows()
-    feed.close()
+    except KeyboardInterrupt:
+        print("Application interrupted by user.")
 
 
 if __name__ == '__main__':
